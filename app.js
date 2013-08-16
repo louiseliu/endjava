2ec5eaa8a10e5351ab3953d9bae1537e5b082042
Qiu Kangbo <kbisnotzombie@gmail.com>
2013-08-16 15:30:29 +0800

    test github

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000); 
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'jade');
 app.engine('jsp',require('endskin').__express);
